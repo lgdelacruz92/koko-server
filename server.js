983e44b800e43432195c4184c59321cbb0180e29
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-11-02 07:44:50 -0400

    Port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const db = require('./db');
 const stateEndpoints = require('./endpoints/state-endpoints');
 const dataEndpoints = require('./endpoints/data-endpoints');
